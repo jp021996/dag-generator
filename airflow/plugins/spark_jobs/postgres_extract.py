ae48751cf928b7bd55d663a24b948b27565c69fc
Joao Amaral <joao.amaral@stone.com.br>
2022-10-25 15:38:39 -0300

    Some changes on docker-compose

--- airflow/plugins/spark_jobs/postgres_extract.py
+++ airflow/plugins/spark_jobs/postgres_extract.py
@@ -1,15 +1,18 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder \
+                    .master("local[*]") \
                     .getOrCreate()
 
 df = spark.read \
     .format("jdbc") \
-    .option("url", "jdbc:postgresql://host.docker.internal:5432/airflow") \
+    .option("url", "jdbc:postgresql://postgres:5432/airflow") \
     .option("dbtable", "public.dag") \
     .option("user", "airflow") \
     .option("password", "airflow") \
     .option("driver", "org.postgresql.Driver") \
     .load()
 
-df.write.parquet("file:///bronze/postgresql/")
+df.printSchema()
+
+df.write.parquet("file:///data/bronze/postgresql/")  
